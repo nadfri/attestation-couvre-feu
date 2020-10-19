d65c33a0bd10bd78f9dce76755ada727be32c0bb
Nader Frigui <nadfri@gmail.com>
2020-10-19 13:02:15 +0200

    nouveau workbox

---
+++ sw.js
@@ -0,0 +1,13 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+if (workbox) {
+  console.log(`WorkBox of Couvre-Feu loaded🎉`);
+  workbox.routing.registerRoute(
+    new RegExp('.+/.+'), //cached all files
+    new workbox.strategies.StaleWhileRevalidate()
+  );
+} else {console.log(`Boo! Workbox didn't load 😬`);}
+
+// This will work!
+
+
