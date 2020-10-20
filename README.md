6efe6b46c1df5e13395129a683eb7916ea438d56
Nader Frigui <nadfri@gmail.com>
2020-10-20 12:39:35 +0200

    tes sw 4.3.1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # attestation-couvre-feu
 Application pour generer une attestation pour le Couvre Feu Octobre 2020
+# Lien de l'application
+https://couvre-feu.netlify.app/
--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
 if (workbox) {
   console.log(`Super ! Workbox est chargé 🎉`);
   
