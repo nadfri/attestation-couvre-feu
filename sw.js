7987f1005046f8d69edd294232a18c0b52955c2b
Nader Frigui <nadfri@gmail.com>
2020-10-22 22:08:28 +0200

    modif starturl en index.html du manifest

--- sw.js
+++ sw.js
@@ -1,11 +1,30 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+console.log("Script sw.js Chargé!");
+
 if (workbox) {
-  console.log(`Super ! Workbox est chargé 🎉`);
-  
+  console.log(`WorkBox of  loaded🎉`);
   workbox.routing.registerRoute(
-    /\.(?:html|js|css|png|jpg|jpeg|svg|gif|ico|json)$/,
+    new RegExp('/.*'), //cached all files
     new workbox.strategies.StaleWhileRevalidate()
   );
-}
 
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+    workbox.routing.registerRoute(
+    ({url}) => url.origin === 'https://fonts.googleapis.com',
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'google-fonts-stylesheets',
+    })
+  );
+  
+  // Cache the underlying font files with a cache-first
+  workbox.routing.registerRoute(
+    ({url}) => url.origin === 'https://fonts.gstatic.com',
+    new workbox.strategies.CacheFirst({
+      cacheName: 'google-fonts-webfonts',
+      
+    })
+  );
+
+} else {console.log(`Boo! Workbox didn't load 😬`);}
 
